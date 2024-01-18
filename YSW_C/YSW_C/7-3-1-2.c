32626a2ffaf6ab9eec047b57118ceb963f77b3e6
kimi5231 <djwjod@naver.com>
2024-01-18 19:06:08 +0900

    7-3-1 문제 풀이

---
+++ YSW_C/YSW_C/7-3-1-2.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int total = 0, num = 0;
+
+	printf("정수 입력(0 to quit): "), scanf("%d", &num);
+	while (num != 0)
+	{
+		total += num;
+		printf("정수 입력(0 to quit): "), scanf("%d", &num);
+	}
+	printf("합계: %d\n", total);
+	return 0;
+}
